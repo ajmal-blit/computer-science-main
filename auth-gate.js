d59c0156c8f4051043c024b1916180dea63fff59
ajmal-blit <vpnmaster480@gmail.com>
2026-07-04 13:56:43 +0530

    Delete auth-gate.js

--- auth-gate.js
+++
@@ -1,4 +0,0 @@
-// Move this out of inline HTML so a strict Content-Security-Policy can block inline scripts.
-if (localStorage.getItem("isLoggedIn") !== "true") {
-    window.location.replace("login.html");
-}
